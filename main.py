1497679e5ccff04c4010c8fc44fcd203b41d5de4
mbek <mmm857436@gmail.com>
2025-11-28 04:08:20 +0000

    cleared

--- main.py
+++ main.py
@@ -53,4 +53,4 @@ app.include_router(import_router.router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8008, reload=True)
